d11f55d722c3577181593d7d5f4c88f146f0258c
Muhammad Shehbaz <shahbazzafar840@gmail.com>
2026-06-15 02:49:08 +0500

    ok

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,5 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI!;
-
-if (!MONGODB_URI) {
-    throw new Error("Please define MONGODB_URI");
-}
-
 let cached = (global as any).mongoose;
 
 if (!cached) {
@@ -14,6 +8,12 @@ if (!cached) {
 
 export async function connectDB() {
     if (cached.conn) return cached.conn;
+
+    const MONGODB_URI = process.env.MONGODB_URI!;
+    
+    if (!MONGODB_URI) {
+        throw new Error("Please define MONGODB_URI");
+    }
 
     if (!cached.promise) {
         cached.promise = mongoose.connect(MONGODB_URI);
